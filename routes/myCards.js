4d6cdd8ae1e2f772f6bb7f31eed68c5c6f381aa2
JuneChen1 <june850713@gmail.com>
2026-09-13 21:24:05 +0800

    refactor(routes): apply isAuth via router.use()

--- routes/myCards.js
+++ routes/myCards.js
@@ -3,9 +3,11 @@ const isAuth = require('../middlewares/isAuth');
 const myCardController = require('../controllers/myCards');
 const router = express.Router();
 
-router.get('/uids', isAuth, myCardController.getUidSummary);
-router.get('/', isAuth, myCardController.getMyCards);
-router.post('/', isAuth, myCardController.updateCards);
-router.delete('/', isAuth, myCardController.deleteUidCards);
+router.use(isAuth);
+
+router.get('/uids', myCardController.getUidSummary);
+router.get('/', myCardController.getMyCards);
+router.post('/', myCardController.updateCards);
+router.delete('/', myCardController.deleteUidCards);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,10 @@ const isAuth = require('../middlewares/isAuth');
 const { authLimiter } = require('../middlewares/limiter');
 const router = express.Router();
 
-router.get('/me', isAuth, userController.getMe);
-router.patch('/me', isAuth, userController.updateMe);
-router.patch(
-  '/me/password',
-  isAuth,
-  authLimiter,
-  userController.updatePassword
-);
+router.use(isAuth);
+
+router.get('/me', userController.getMe);
+router.patch('/me/password', authLimiter, userController.updatePassword);
+router.patch('/me', userController.updateMe);
 
 module.exports = router;
