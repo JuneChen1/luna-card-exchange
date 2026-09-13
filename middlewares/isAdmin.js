87095b2110b4ca96dff3f2923c21d49a46e46db1
JuneChen1 <june850713@gmail.com>
2026-09-13 21:20:53 +0800

    refactor(isAdmin): reuse isAuth middleware

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,31 +1,16 @@
-const jwt = require('jsonwebtoken');
-const { dataSource } = require('../db/data-source');
+const isAuth = require('./isAuth');
 const appError = require('../utils/appError');
 
-async function isAdmin(req, res, next) {
-  try {
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      return next(appError(401, '請先登入'));
+function isAdmin(req, res, next) {
+  isAuth(req, res, (err) => {
+    if (err) return next(err);
+
+    if (req.user.role !== 'ADMIN') {
+      return next(appError(403, '您沒有權限執行此操作'));
     }
 
-    const token = authHeader.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const userRepo = dataSource.getRepository('Users');
-    const user = await userRepo.findOneBy({ id: decoded.id });
-    if (!user) return next(appError(401, '無效的 token'));
-
-    if (user.role !== 'ADMIN')
-      return next(appError(403, '您沒有權限執行此操作'));
-
-    req.user = user;
     next();
-  } catch (error) {
-    if (error.name === 'TokenExpiredError')
-      return next(appError(401, 'Token 已過期'));
-
-    next(appError(401, '無效的 token'));
-  }
+  });
 }
 
 module.exports = isAdmin;
