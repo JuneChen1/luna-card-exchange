4215966f7588d8e85bf73b438240b4773851a6b2
JuneChen1 <june850713@gmail.com>
2026-09-19 17:01:41 +0800

    fix(mailer): force IPv4 and add conn timeout

--- utils/mailer.js
+++ utils/mailer.js
@@ -4,6 +4,8 @@ const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: Number(process.env.MAIL_PORT) || 587,
   secure: Number(process.env.MAIL_PORT) === 465,
+  family: 4,
+  connectionTimeout: 5000,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
